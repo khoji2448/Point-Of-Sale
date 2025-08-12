ef94ea4770f593e043ca5d91de9eb3998511675b
Ahmad Mujtaba <amujtaba527@gmail.com>
2025-08-12 21:05:23 +0500

    product description removed

--- src/app/(screens)/product/page.tsx
+++ src/app/(screens)/product/page.tsx
@@ -251,7 +251,7 @@ const Products = () => {
                 isDisabled={isEditMode && editLoading} // Disable during edit loading
               />
 
-              {/* Product Name */}
+              {/* Product Name
               <label htmlFor="name">Product Name</label>
               <input
                 type="text"
@@ -262,7 +262,7 @@ const Products = () => {
                 required
                 className="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-200"
                 disabled={isEditMode && editLoading} // Disable during edit loading
-              />
+              /> */}
 
               {/* Description */}
               <label htmlFor="description">Product Description</label>
