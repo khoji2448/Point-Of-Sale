5bd5c2a45ea0116b8d6da337c59e01840a0648e7
Ahmad Mujtaba <khoji2448@gmail.com>
2025-11-05 14:36:17 +0500

    updated product demand

--- src/app/api/product/demand/route.ts
+++ src/app/api/product/demand/route.ts
@@ -4,7 +4,7 @@ import { pool } from "@/lib/db";
 export async function GET() {
   try {
       console.log("Fetching products...");
-      const res = await pool.query("SELECT * FROM products WHERE stock < min_stock_level ORDER BY id ASC");
+      const res = await pool.query("SELECT * FROM products WHERE stock <= min_stock_level ORDER BY id ASC");
       return NextResponse.json(res.rows);
   } catch (error) {
     return NextResponse.json({ error: "Error fetching products" + error }, { status: 500 });
